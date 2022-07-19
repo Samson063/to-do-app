2e5eb8686dc476febde0a25a4f969bd31236c86b
Samson063 <furthermaths078@gamil.com>
2022-07-19 01:58:13 +0100

    to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  updateInput(key, value){
+
+    this.setState({
+      [key]: value
+    })
+  }
+  addItem(){
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    const list = [...this.state.list];
+
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+  deleteItem(id){
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({list: updatedList});
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>
+        <h1>Add an Item...</h1>
+        <br/>
+        <input
+        type='text'
+        placeholder='Type item here...'
+        value={this.newItem}
+        onChange={e => this.updateInput( "newItem", e.target.value)}
+        />
+        <button className='addItem' onClick={() => this.addItem()}>
+          Add
+        </button>
+        <br/>
+        <ul>{this.state.list.map(item =>{
+          return(
+            <li key={item.id}>
+              {item.value}
+              <button className='delete' onClick={() => this.deleteItem(item.id)}>
+                x
+              </button>
+            </li>
+          )
+        })}</ul>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
